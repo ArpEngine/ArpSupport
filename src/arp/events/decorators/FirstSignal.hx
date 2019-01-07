eedc8d151908f220c8f46d38ecd7cd51d7d1f37c
kaikoga <info@mail.kaikoga.net>
2019-01-08 02:37:55 +0900

    add IArpSignalOut.first()

---
+++ src/arp/events/decorators/FirstSignal.hx
@@ -0,0 +1,22 @@
+package arp.events.decorators;
+
+class FirstSignal<T> implements IArpSignalOut<T> {
+
+	private var source:IArpSignalOut<T>;
+	private var output:ArpSignal<T>;
+
+	public function new(source:IArpSignalOut<T>) {
+		this.output = new ArpSignal<T>();
+		this.source = source;
+		this.source.push(this.handler);
+	}
+
+	private function handler(event:T):Void {
+		this.source.remove(this.handler);
+		this.output.dispatch(event);
+	}
+
+	public function push(handler:T->Void):Int return this.output.push(handler);
+	public function remove(handler:T->Void):Bool return this.output.remove(handler);
+	public function flush():Void return this.output.flush();
+}
