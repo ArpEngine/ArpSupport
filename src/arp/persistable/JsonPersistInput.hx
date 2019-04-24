7ee624253f98b44bc76a24437497b513f90de7cd
kaikoga <info@mail.kaikoga.net>
2019-04-24 23:20:09 +0900

    JsonPersistIo takes composition pattern

--- src/arp/persistable/JsonPersistInput.hx
+++ src/arp/persistable/JsonPersistInput.hx
@@ -4,21 +4,55 @@ import haxe.crypto.Base64;
 import haxe.io.Bytes;
 import haxe.Json;
 
-class JsonPersistInput extends JsonPersistInputBase {
+@:access(arp.persistable.AnonPersistInput._data)
 
-	public function new(data:String, persistLevel:Int = 0) {
-		super(Json.parse(data), persistLevel);
+class JsonPersistInput implements IPersistInput {
+
+	private var input:IPersistInput;
+	private var anon:AnonPersistInput;
+
+	private var _persistLevel:Int = 0;
+	public var persistLevel(get, never):Int;
+	inline private function get_persistLevel():Int return this._persistLevel;
+
+	public function new(json:String, persistLevel:Int = 0) {
+		var data = Json.parse(json);
+		this.anon = new AnonPersistInput(data, persistLevel);
+		this.input = anon;
+		this._persistLevel = persistLevel;
 	}
+
+	public function readBlob(name:String):Bytes {
+		return Base64.decode(Reflect.field(this.anon._data, name));
+	}
+	public function nextBlob():Bytes return this.readBlob(@:privateAccess this.anon.nextName());
+
+	public function readPersistable<T:IPersistable>(name:String, persistable:T):T {
+		this.readEnter(name);
+		persistable.readSelf(this);
+		this.readExit();
+		return persistable;
+	}
+
+	public function readEnter(name:String):Void this.input.readEnter(name);
+	public function readListEnter(name:String):Void this.input.readListEnter(name);
+	public function readExit():Void this.input.readExit();
+	public function readScope(name:String, body:IPersistInput->Void):Void this.input.readScope(name, body);
+	public function readListScope(name:String, body:IPersistInput->Void):Void this.input.readListScope(name, body);
+
+	public function readNameList(name:String):Array<String> return this.input.readNameList(name);
+
+	public function readBool(name:String):Bool return this.input.readBool(name);
+	public function readInt32(name:String):Int return this.input.readInt32(name);
+	public function readUInt32(name:String):UInt return this.input.readUInt32(name);
+	public function readDouble(name:String):Float return this.input.readDouble(name);
+
+	public function readUtf(name:String):String return this.input.readUtf(name);
+
+	public function nextBool():Bool return this.input.nextBool();
+	public function nextInt32():Int return this.input.nextInt32();
+	public function nextUInt32():UInt return this.input.nextUInt32();
+	public function nextDouble():Float return this.input.nextDouble();
+
+	public function nextUtf():String return this.input.nextUtf();
 }
-
-private class JsonPersistInputBase extends AnonPersistInput {
-
-	private function new(data:Dynamic, persistLevel:Int = 0) {
-		super(data, persistLevel);
-	}
-
-	override public function readBlob(name:String):Bytes {
-		return Base64.decode(Reflect.field(this._data, name));
-	}
-}
-
--- src/arp/persistable/JsonPersistOutput.hx
+++ src/arp/persistable/JsonPersistOutput.hx
@@ -4,17 +4,55 @@ import haxe.crypto.Base64;
 import haxe.io.Bytes;
 import haxe.Json;
 
-class JsonPersistOutput extends AnonPersistOutput {
+class JsonPersistOutput implements IPersistOutput {
+
+	private var output:IPersistOutput;
+	private var anon:AnonPersistOutput;
+
+	private var _persistLevel:Int = 0;
+	public var persistLevel(get, never):Int;
+	inline private function get_persistLevel():Int return this._persistLevel;
 
 	public var json(get, never):String;
-	private function get_json():Dynamic return Json.stringify(this.data);
+	private function get_json():Dynamic return Json.stringify(this.anon.data);
 
 	public function new(persistLevel:Int = 0) {
-		super(null, persistLevel);
+		this.anon = new AnonPersistOutput(null, persistLevel);
+		this.output = anon;
+		this._persistLevel = persistLevel;
 	}
 
-	override public function writeBlob(name:String, bytes:Bytes):Void {
-		Reflect.setField(this._data, name, Base64.encode(bytes));
+	public function writeBlob(name:String, bytes:Bytes):Void {
+		Reflect.setField(this.anon.data, name, Base64.encode(bytes));
 	}
+	public function pushBlob(bytes:Bytes):Void this.writeBlob(@:privateAccess this.anon.nextName(), bytes);
+
+	public function writePersistable(name:String, persistable:IPersistable):Void {
+		this.writeEnter(name);
+		persistable.writeSelf(this);
+		this.writeExit();
+	}
+
+	public function writeEnter(name:String):Void this.output.writeEnter(name);
+	public function writeListEnter(name:String):Void this.output.writeListEnter(name);
+	public function writeExit():Void this.output.writeExit();
+	public function writeScope(name:String, body:IPersistOutput->Void):Void this.output.writeScope(name, body);
+	public function writeListScope(name:String, body:IPersistOutput->Void):Void this.output.writeListScope(name, body);
+
+	public function writeNameList(name:String, value:Array<String>):Void this.output.writeNameList(name, value);
+
+	public function writeBool(name:String, value:Bool):Void this.output.writeBool(name, value);
+	public function writeInt32(name:String, value:Int):Void this.output.writeInt32(name, value);
+	public function writeUInt32(name:String, value:UInt):Void this.output.writeUInt32(name, value);
+	public function writeDouble(name:String, value:Float):Void this.output.writeDouble(name, value);
+
+	public function writeUtf(name:String, value:String):Void this.output.writeUtf(name, value);
+
+	public function pushBool(value:Bool):Void this.output.pushBool(value);
+	public function pushInt32(value:Int):Void this.output.pushInt32(value);
+	public function pushUInt32(value:UInt):Void this.output.pushUInt32(value);
+	public function pushDouble(value:Float):Void this.output.pushDouble(value);
+
+	public function pushUtf(value:String):Void this.output.pushUtf(value);
 }
 
