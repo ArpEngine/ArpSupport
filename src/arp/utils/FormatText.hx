f395a32ddeb7ee0a910249c62436b41042b2751f
kaikoga <info@mail.kaikoga.net>
2020-02-20 23:24:13 +0900

    remove full width support

--- src/arp/utils/FormatText.hx
+++ src/arp/utils/FormatText.hx
@@ -51,7 +51,6 @@ private class ParametrizedNode implements INode {
 	private var _flagAlign:String = "l";
 	private var _flagDigits:Int = 0;
 	private var _flagPrecision:Int = 0;
-	private var _flagFormat:String = "s";
 	private var _default:String = " ";
 
 	public function new(value:String) {
@@ -59,15 +58,10 @@ private class ParametrizedNode implements INode {
 		var array:Array<String> = value.substr(1, value.length - 2).split(":");
 		if (array[0] != null) this._name = array[0];
 		if (array[1] != null) {
-			for (flag in new ERegIterator(~/[0-9]+|\.[0-9]+|[lrcsz]/, array[1])) {
+			for (flag in new ERegIterator(~/[0-9]+|\.[0-9]+|[lrc]/, array[1])) {
 				switch (flag.charAt(0)) {
 					case "l", "r", "c":
 						this._flagAlign = flag;
-					case "s":
-						this._flagFormat = flag;
-					case "z":
-						this._flagFormat = flag;
-						this._default = "　";
 					case ".":
 						this._flagPrecision = Std.parseInt(flag.substr(1));
 					default:
@@ -78,28 +72,11 @@ private class ParametrizedNode implements INode {
 		if (array[2] != null) this._default = array[2];
 	}
 
-	private static function toZenChar(f:EReg):String {
-		return "０１２３４５６７８９".charAt(Std.parseInt(f.matched(0)));
-	}
-
-	public static function toZenString(value:Float):String {
-		return ~/./.map(Std.string(value), toZenChar);
-	}
-
 	public function publishSelf(params:String->Any, customFormatter:Any->String):String {
 		if (params == null) return this.value;
 
 		var param:Any = params(this._name);
 		var str:String = customFormatter(param);
-		if (str == null) {
-			if (Std.is(param, Float)) {
-				switch (this._flagFormat) {
-					case "z":
-						str = toZenString(param);
-					case _:
-				}
-			}
-		}
 		if (str == null) str = Std.string(param);
 
 		switch (this._flagAlign) {
