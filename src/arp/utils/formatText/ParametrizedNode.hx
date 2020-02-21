81d25782d888551f7f7c60a173e705abe52eecd5
kaikoga <info@mail.kaikoga.net>
2020-02-21 09:02:25 +0900

    let space for more flags

--- src/arp/utils/formatText/ParametrizedNode.hx
+++ src/arp/utils/formatText/ParametrizedNode.hx
@@ -2,51 +2,62 @@ package arp.utils.formatText;
 
 import arp.utils.FormatText.TCustomFormatter;
 import arp.utils.FormatText.TFormatParams;
-import arp.iterators.ERegIterator;
 
 class ParametrizedNode implements INode {
 
 	private var value:String;
-	private var _name:String = "";
-	private var _flagAlign:String = "l";
-	private var _flagDigits:Int = 0;
-	private var _flagPrecision:Int = 0;
-	private var _default:String = " ";
+	private var name:String = "";
+	private var digits:Int = 0;
+	private var precision:Int = 0;
+	private var flags:String = "";
+	private var defaultValue:String = "";
+
+	private var flagAlign(get, never):String;
+	private function get_flagAlign():String {
+		var flagAlign:String = "l";
+		if (this.flags.indexOf("r") >= 0) flagAlign = "r";
+		if (this.flags.indexOf("c") >= 0) flagAlign = "c";
+		return flagAlign;
+	}
 
 	public function new(value:String) {
 		this.value = value;
 		var array:Array<String> = value.substr(1, value.length - 2).split(":");
-		if (array[0] != null) this._name = array[0];
+		if (array[0] != null) this.name = array[0];
 		if (array[1] != null) {
-			for (flag in new ERegIterator(~/[0-9]+|\.[0-9]+|[lrc]/, array[1])) {
-				switch (flag.charAt(0)) {
-					case "l", "r", "c":
-						this._flagAlign = flag;
-					case ".":
-						this._flagPrecision = Std.parseInt(flag.substr(1));
-					default:
-						this._flagDigits = Std.parseInt(flag);
-				}
+			var ereg = ~/([0-9]*)(\.[0-9]+)?([a-zA-Z]*)/;
+			while (ereg.match(array[1])) {
+				var digits:String = ereg.matched(1);
+				var precision:String = ereg.matched(2);
+				var flags:String = ereg.matched(3);
+				if (digits.length > 0) this.digits = Std.parseInt(digits);
+				if (precision.length > 0) this.precision = Std.parseInt(precision);
+				if (flags.length > 0) this.flags += flags;
 			}
 		}
-		if (array[2] != null) this._default = array[2];
+		if (array[2] != null) this.defaultValue = array.splice(2, array.length - 2).join(":");
 	}
 
 	public function publishSelf(params:TFormatParams, customFormatter:TCustomFormatter):String {
 		if (params == null) return this.value;
 
-		var param:Any = params(this._name);
-		var str:String = customFormatter(param);
-		if (str == null) str = Std.string(param);
+		var param:Any = params(this.name);
+		var str:String = null;
+		if (param == null) {
+			str = this.defaultValue;
+		} else {
+			str = customFormatter(param);
+			if (str == null) str = Std.string(param);
+		}
 
-		switch (this._flagAlign) {
+		switch (this.flagAlign) {
 			case "l":
-				while (str.length < this._flagDigits) str += this._default;
+				while (str.length < this.digits) str += " ";
 			case "r":
-				while (str.length < this._flagDigits) str = this._default + str;
+				while (str.length < this.digits) str = " " + str;
 			case "c":
 				var b:Bool = false;
-				while (str.length < this._flagDigits) str = (b = !b) ? (str + this._default) : (this._default + str);
+				while (str.length < this.digits) str = (b = !b) ? (str + " ") : (" " + str);
 		}
 		return str;
 	}
