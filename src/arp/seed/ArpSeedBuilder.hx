8337990a4ae7aaf34beb64f4d7a2f96f3f5a484c
kaikoga <info@mail.kaikoga.net>
2020-11-03 17:07:53 +0900

    add some convenience methods

--- src/arp/seed/ArpSeedBuilder.hx
+++ src/arp/seed/ArpSeedBuilder.hx
@@ -7,41 +7,41 @@ import arp.seed.sources.IArpSeedSource;
 abstract ArpSeedBuilder(ArpSeed) {
 
 	public var seedName(get, set):String;
-	private function get_seedName():String return this.seedName;
-	private function set_seedName(value:String):String return @:privateAccess this.seedName = value;
+	inline private function get_seedName():String return this.seedName;
+	inline private function set_seedName(value:String):String return @:privateAccess this.seedName = value;
 	public var key(get, set):String;
-	private function get_key():String return this.key;
-	private function set_key(value:String):String return @:privateAccess this.key = value;
+	inline private function get_key():String return this.key;
+	inline private function set_key(value:String):String return @:privateAccess this.key = value;
 	public var env(get, set):ArpSeedEnv;
-	private function get_env():ArpSeedEnv return this.env;
-	private function set_env(value:ArpSeedEnv):ArpSeedEnv return @:privateAccess this.env = value;
+	inline private function get_env():ArpSeedEnv return this.env;
+	inline private function set_env(value:ArpSeedEnv):ArpSeedEnv return @:privateAccess this.env = value;
 
 	public var className(get, set):Null<String>;
-	private function get_className():Null<String> return this.className;
-	private function set_className(value:Null<String>):Null<String> return @:privateAccess this.className = value;
+	inline private function get_className():Null<String> return this.className;
+	inline private function set_className(value:Null<String>):Null<String> return @:privateAccess this.className = value;
 	public var name(get, set):Null<String>;
-	private function get_name():Null<String> return this.name;
-	private function set_name(value:Null<String>):Null<String> return @:privateAccess this.name = value;
+	inline private function get_name():Null<String> return this.name;
+	inline private function set_name(value:Null<String>):Null<String> return @:privateAccess this.name = value;
 	public var heat(get, set):Null<String>;
-	private function get_heat():Null<String> return this.heat;
-	private function set_heat(value:Null<String>):Null<String> return @:privateAccess this.heat = value;
+	inline private function get_heat():Null<String> return this.heat;
+	inline private function set_heat(value:Null<String>):Null<String> return @:privateAccess this.heat = value;
 
 	public var maybeRef(get, set):Bool;
-	private function get_maybeRef():Bool return this.maybeRef;
-	private function set_maybeRef(value:Bool):Bool return @:privateAccess this.maybeRef = value;
+	inline private function get_maybeRef():Bool return this.maybeRef;
+	inline private function set_maybeRef(value:Bool):Bool return @:privateAccess this.maybeRef = value;
 	public var simpleValue(get, set):Null<String>;
-	private function get_simpleValue():Null<String> return @:privateAccess this.simpleValue;
-	private function set_simpleValue(value:Null<String>):Null<String> return @:privateAccess this.simpleValue = value;
+	inline private function get_simpleValue():Null<String> return @:privateAccess this.simpleValue;
+	inline private function set_simpleValue(value:Null<String>):Null<String> return @:privateAccess this.simpleValue = value;
 	public var children(get, set):Null<Array<ArpSeed>>;
-	private function get_children():Null<Array<ArpSeed>> return @:privateAccess this.children;
-	private function set_children(value:Null<Array<ArpSeed>>):Null<Array<ArpSeed>> return @:privateAccess this.children = value;
+	inline private function get_children():Null<Array<ArpSeed>> return @:privateAccess this.children;
+	inline private function set_children(value:Null<Array<ArpSeed>>):Null<Array<ArpSeed>> return @:privateAccess this.children = value;
 
 	public var source(get, set):Null<IArpSeedSource>;
-	private function get_source():Null<IArpSeedSource> return this.source;
-	private function set_source(value:Null<IArpSeedSource>):Null<IArpSeedSource> return @:privateAccess this.source = value;
+	inline private function get_source():Null<IArpSeedSource> return this.source;
+	inline private function set_source(value:Null<IArpSeedSource>):Null<IArpSeedSource> return @:privateAccess this.source = value;
 
 	public var value(get, set):Null<String>;
-	private function get_value():Null<String> return this.value;
+	inline private function get_value():Null<String> return this.value;
 	private function set_value(value:Null<String>):Null<String> {
 		return if (children != null) {
 			for (child in children) {
@@ -61,8 +61,22 @@ abstract ArpSeedBuilder(ArpSeed) {
 
 	public function amend():Iterator<IListAmendCursor<ArpSeed>> {
 		var list:ArrayList<ArpSeed> = new ArrayList<ArpSeed>();
-		@:privateAccess list.value = this.children;
+		@:privateAccess list.value = children;
 		return list.amend();
+	}
+
+	public function append(seed:ArpSeed):Void {
+		if (children == null) children = [];
+		children.push(seed);
+	}
+
+	public function prepend(seed:ArpSeed):Void {
+		if (children == null) children = [];
+		children.unshift(seed);
+	}
+
+	public function remove(seed:ArpSeed):Bool {
+		return if (children == null) false else children.remove(seed);
 	}
 
 	public function toSeed():ArpSeed return this;
